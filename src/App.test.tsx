26c8283f28c50313a04b453ca96316662157cca3
Zett-8 <kame0008@gmail.com>
2020-11-24 15:47:09 +0100

    added basic test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('when everything is ok', () => {
+  beforeEach(() => {
+    render(<App />)
+  })
+
+  test('should render the app component without crashing', () => {
+    screen.debug()
+  })
+
+  test('should select the children that is being passed to the CustomInput component', () => {
+    screen.getByText('input:') // implicit assertion
+  })
+
+  test('should select the input element by its role', () => {
+    screen.getByRole('textbox')
+  })
+
+  test('should select the label element by its text', () => {
+    screen.getByLabelText('input:')
+  })
+})
